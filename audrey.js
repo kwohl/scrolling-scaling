e605af3e48e971f08e07d1c06825ad984fd98f3c
Kathryn Wohl <ktwohl@gmail.com>
2020-01-22 14:51:28 -0600

    added event listener that adjusts size of box while scrolling

--- audrey.js
+++ audrey.js
@@ -1,19 +1,27 @@
 const audrey = document.getElementById("audrey")
 
+
 /*
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-.addEventListener("", function () {
+document.addEventListener("scroll", function () {
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
     */
-
+   let y = window.scrollY;
+   audrey.style.minWidth = '50px';
+   audrey.style.width = `${y/3}px`;
 
 
     /*
         Adjust the height of audrey to be 1/4 the value of
         `window.scrollY`. No lower than 100px, though.
     */
+   audrey.style.minHeight = '100px';
+   audrey.style.height = `${y/4}px`;
 })
+
+
+
